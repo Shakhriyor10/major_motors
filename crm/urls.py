54e773557bc5d33f98395c8d087b6fb60058ef7b
Shakhriyor10 <nnnnafisa@gmail.com>
2026-01-11 10:47:20 +0500

    sdadasdasd

--- crm/urls.py
+++ crm/urls.py
@@ -4,5 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('roles/', views.roles, name='roles'),
+    path('customers/', views.customers, name='customers'),
+    path('leads/', views.leads, name='leads'),
+    path('inventory/', views.inventory, name='inventory'),
+    path('deals/', views.deals, name='deals'),
     path('cash/', views.cash_dashboard, name='cash-dashboard'),
 ]
