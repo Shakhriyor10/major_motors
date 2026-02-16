868bcebc3104c6387293dede14ef0dbc485435af
Shakhriyor10 <77412906+Shakhriyor10@users.noreply.github.com>
2026-02-16 19:06:48 +0500

    Restrict attorney tab access by user permission

--- crm/tests.py
+++ crm/tests.py
@@ -1,3 +1,38 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Permission
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class AutosalonAttorneyAccessTests(TestCase):
+    def setUp(self):
+        self.user_model = get_user_model()
+        self.url = reverse('autosalon')
+
+    def test_attorney_tab_hidden_without_permission(self):
+        user = self.user_model.objects.create_user(username='regular', password='pass1234')
+        self.client.force_login(user)
+
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response, 'autosalon-attorney-tab')
+
+    def test_attorney_tab_visible_with_permission(self):
+        user = self.user_model.objects.create_user(username='allowed', password='pass1234')
+        permission = Permission.objects.get(codename='view_powerofattorney')
+        user.user_permissions.add(permission)
+        self.client.force_login(user)
+
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'autosalon-attorney-tab')
+
+    def test_save_attorney_forbidden_without_permission(self):
+        user = self.user_model.objects.create_user(username='forbidden', password='pass1234')
+        self.client.force_login(user)
+
+        response = self.client.post(self.url, {'action': 'save_attorney'})
+
+        self.assertEqual(response.status_code, 403)
